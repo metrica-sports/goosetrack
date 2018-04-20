c14b789bcdd013764aa901359c7b0e38f6f0deef
ndelvalle <nicolas.delvalle@gmail.com>
2018-04-20 15:02:51 -0300

    Implement created at and updated at props and add eslint/prettier

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+const dotProp = require('dot-prop')
+
 function isReferenceInQuery(query, reference) {
+  if (dotProp.has(query, reference)) {
+    return true
+  }
   if (typeof query[reference] !== 'undefined') {
     return true
   }
-  return query.$set && query.set[reference]
+  return query.$set && query.$set[reference] !== 'undefined'
 }
 
-function trackPlugin(schema, options) {
-  const props = options.map((option) => ({
-    reference: option.reference,
-    name: option.name || `${option.reference}UpdatedAt`,
-  }))
-
-  props.forEach(({ reference, name }) => {
+function trackProperties(schema, properties) {
+  properties.forEach(({ reference, name }) => {
     if (!schema.path(name)) {
       schema.add({ [name]: { type: Date, index: true } })
     }
@@ -24,25 +24,71 @@ function trackPlugin(schema, options) {
     })
 
     schema.pre('findOneAndUpdate', function preFindOneAndUpdate(next) {
-      if (isReferenceInQuery(this._update, reference)) {
+      if (isReferenceInQuery(this.getUpdate(), reference)) {
         this.update({}, { [name]: new Date() })
       }
       next()
     })
 
     schema.pre('update', function preUpdate(next) {
-      if (isReferenceInQuery(this._update, reference)) {
+      if (isReferenceInQuery(this.getUpdate(), reference)) {
         this.update({}, { [name]: new Date() })
       }
       next()
     })
   })
+}
 
-  if (!schema.methods.touchProperty) {
-    schema.methods.touchProperty = function touchProperty(propery, cb) {
-      this[propery] = new Date()
-      this.save(cb)
+function trackPlugin(schema, options) {
+  const updatedAt =
+    options.updatedAt === 'undefined' ? 'updatedAt' : options.updatedAt
+  const createdAt =
+    options.createdAt === 'undefined' ? 'createdAt' : options.createdAt
+  const properties = options.properties || []
+
+  const props = properties.map((option) => ({
+    reference: option.reference,
+    name: option.name || `${option.reference}UpdatedAt`,
+  }))
+
+  trackProperties(schema, props)
+
+  if (!updatedAt && !createdAt) {
+    return
+  }
+
+  if (updatedAt && !schema.path(updatedAt)) {
+    schema.add({ [updatedAt]: { type: Date, index: true } })
+  }
+  if (createdAt && !schema.path(createdAt)) {
+    schema.add({ [createdAt]: { type: Date, index: true } })
+  }
+
+  schema.pre('save', function preSave(next) {
+    const now = new Date()
+    if (this.isNew) {
+      if (createdAt) {
+        this[createdAt] = now
+      }
+      if (updatedAt) {
+        this[updatedAt] = now
+      }
+    } else if (updatedAt && this.isModified()) {
+      this[updatedAt] = now
     }
+    next()
+  })
+
+  if (updatedAt) {
+    schema.pre('update', function preUpdate(next) {
+      this.update({}, { [updatedAt]: new Date() })
+      next()
+    })
+
+    schema.pre('findOneAndUpdate', function preFindOneAndUpdate(next) {
+      this.findOneAndUpdate({}, { [updatedAt]: new Date() })
+      next()
+    })
   }
 }
 
